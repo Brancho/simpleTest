0631546deed60ea460ba418c6541aeee5f393ec4
Branka M <Brancho@Brankas-MacBook-Air.local>
2016-02-23 15:22:30 +0100

    done

--- task.js
+++ task.js
@@ -2,22 +2,98 @@ var App = (function (document, XMLHttpRequest) {
   var slike = document.getElementById("photos");
   var apiKey = 'a66205f0e96fff74cff92e5ed60e7ba4';
   var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + apiKey + "&per_page=&page=&format=json&nojsoncallback=1";
-  var link = function(index, farm, server, id, secret) {
-    var slika = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + ".jpg"
-    return "<li class='row'><a class='thumbnail' href='#'><img class='img-"+index+"' class='group1' src ='" + slika + "'onclick='App.enlargePhoto(event, "+index+")'/></a></li>";
-  }
   var photos;
   var activePhotoIndex;
 
 
+  function createPhotoElement(index, photoData) {
+    var photoUrl = "https://farm" + photoData.farm + ".staticflickr.com/" + photoData.server + "/" + photoData.id + "_" + photoData.secret + ".jpg";
 
-function createPhoto(data){
-  photos = data.photos.photo;
-  for (var i = 0; i < photos.length; i++) {
-    slike.innerHTML = slike.innerHTML + link(i, photos[i].farm, photos[i].server, photos[i].id, photos[i].secret);
-    getPhotoLocation(photos[i].id);
-  };
-}
+    var bodyDiv = document.createElement('DIV');
+    bodyDiv.classList.add('container');
+    bodyDiv.id = 'photos';
+
+    var overlayDiv = document.createElement('DIV');
+    overlayDiv.classList.add('overlay');
+
+    var activePhoto = document.createElement('IMG');
+    activePhoto.src = "";
+    activePhoto.classList.add('active-photo');
+
+    var left = document.createElement('IMG');
+    left.src = "left.png";
+    left.classList.add('left');
+    left.addEventListener('click', App.previous);
+
+    var right = document.createElement('IMG');
+    right.src = "right.png";
+    right.classList.add('right');
+    right.addEventListener('click', App.next);
+
+    var exit = document.createElement('IMG');
+    exit.src = "x.png";
+    exit.classList.add('exit');
+    exit.addEventListener('click', App.exit);
+
+    var wrapperDiv = document.createElement('DIV');
+    wrapperDiv.classList.add('photo-wrapper');
+
+    var newImg = document.createElement('IMG');
+    newImg.src = photoUrl;
+    newImg.addEventListener('click', App.enlargePhoto);
+    newImg.classList.add('img-' + index);
+
+     var button = document.createElement('BUTTON');
+     button.innerHTML = "flipThePhoto";
+     button.addEventListener('click', App.flip);
+
+    var titleEl = document.createElement('P');
+    titleEl.innerHTML = 'Title: ' + photoData.title;
+
+    var ownerEl = document.createElement('P');
+    ownerEl.innerHTML = 'Owner: ' + photoData.owner;
+
+    var geoLocation = document.createElement('P');
+    geoLocation.innerHTML = 'GeoLocation: ' + photoData.location;
+
+    var flipper = document.createElement('DIV');
+    flipper.classList.add('flipper');
+
+    var front = document.createElement('DIV');
+    front.classList.add('front');
+
+    var back = document.createElement('DIV');
+    back.classList.add('back');
+
+
+    document.body.appendChild(bodyDiv);
+    bodyDiv.appendChild(overlayDiv);
+    overlayDiv.appendChild(activePhoto);
+    overlayDiv.appendChild(left);
+    overlayDiv.appendChild(right);
+    overlayDiv.appendChild(exit);
+    wrapperDiv.appendChild(flipper);
+    flipper.appendChild(front);
+    flipper.appendChild(back);
+    back.appendChild(titleEl);
+    back.appendChild(ownerEl);
+    back.appendChild(geoLocation);
+    front.appendChild(newImg);
+    wrapperDiv.appendChild(button);
+
+
+
+    document.querySelector('.container').appendChild(wrapperDiv);
+  }
+
+
+  function createPhoto(data){
+    photos = data.photos.photo;
+    for (var i = 0; i < photos.length; i++) {
+      getPhotoLocation(i, photos[i]);
+
+    };
+  }
 
 
   function loadDoc() {
@@ -32,14 +108,17 @@ function createPhoto(data){
     xhttp.send();
   }
 
-  function getPhotoLocation(photoId) {
-    var photoUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=" +apiKey+ "&photo_id=" +photoId+ "&format=json&nojsoncallback=1";
+  function getPhotoLocation(index, photo) {
+    var photoUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=" +apiKey+ "&photo_id=" +photo.id+ "&format=json&nojsoncallback=1";
 
     var xhttpForLocation = new XMLHttpRequest();
     xhttpForLocation.onreadystatechange = function() {
       if (xhttpForLocation.readyState == 4 && xhttpForLocation.status == 200) {
         var data = JSON.parse(xhttpForLocation.responseText);
+        photo.location = data.message;
+        createPhotoElement(index, photo);
       }
+
     }
     xhttpForLocation.open("GET", photoUrl, true);
     xhttpForLocation.send();
@@ -50,14 +129,17 @@ function createPhoto(data){
     document.querySelector('.overlay').style.display = 'block';
     document.querySelector('.active-photo').src = currentCardElement.src;
     activePhotoIndex = Number(currentCardElement.className.slice(4));
+    console.log(activePhotoIndex);
   }
 
   function exit() {
     document.querySelector('.overlay').style.display = 'none';
-    currentCardElement.classList.remove('active-card');
   }
 
   function next() {
+    if (activePhotoIndex == photos.length - 1) {
+      return;
+    }
     var nextPhoto = photos[activePhotoIndex + 1];
     var photoUrl = "https://farm" + nextPhoto.farm + ".staticflickr.com/" + nextPhoto.server + "/" + nextPhoto.id + "_" + nextPhoto.secret + ".jpg";
     document.querySelector('.active-photo').src = photoUrl;
@@ -65,18 +147,30 @@ function createPhoto(data){
   }
 
   function previous() {
+    if (activePhotoIndex == 0) {
+      return;
+    }
     var previousPhoto = photos[activePhotoIndex - 1];
     var photoUrl = "https://farm" + previousPhoto.farm + ".staticflickr.com/" + previousPhoto.server + "/" + previousPhoto.id + "_" + previousPhoto.secret + ".jpg";
     document.querySelector('.active-photo').src = photoUrl;
     activePhotoIndex --;
   }
 
+   function flip(event) {
+     var currentPhoto = event.target.closest('.photo-wrapper');
+     currentPhoto.classList.toggle('clicked');
+   }
+
   return {
     loadDoc: loadDoc,
     enlargePhoto: enlargePhoto,
     exit: exit,
     next: next,
-    previous: previous
+    previous: previous,
+    flip: flip
+
   }
 
 })(window.document, window.XMLHttpRequest);
+
+document.addEventListener("DOMContentLoaded", App.loadDoc);
